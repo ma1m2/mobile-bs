a1fcd9f68b4c0663aa6932e564bd557c381f49b9
Sveta <ma1m2@mail.ru>
2026-05-18 18:08:04 +0300

    test findJavaSearchTest doesn't open the atricle

--- src/test/java/msl/qa/tests/WikiSearchTests.java
+++ src/test/java/msl/qa/tests/WikiSearchTests.java
@@ -1,18 +1,18 @@
 package msl.qa.tests;
 
+import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
-import static io.appium.java_client.AppiumBy.accessibilityId;
 import static io.appium.java_client.AppiumBy.id;
 import static io.qameta.allure.Allure.step;
 
 public class WikiSearchTests  extends TestBase{
 
   @Test
-  void successfulSearchTest() throws Exception {
+  void successfulSearchTest(){
 
     step("Type search", () -> {
       $(id("org.wikipedia.alpha:id/search_container")).click();
@@ -23,6 +23,25 @@ public class WikiSearchTests  extends TestBase{
       $$(id("org.wikipedia.alpha:id/page_list_item_title"))
               .shouldHave(sizeGreaterThan(0));
     });
+
+  }
+
+  @Test
+  void findJavaSearchTest(){
+
+    step("Type search", () -> {
+      $(id("org.wikipedia.alpha:id/search_container")).click();
+      $(id("org.wikipedia.alpha:id/search_src_text")).sendKeys("Java");
+    });
+
+    step("Open first article of list", () -> {
+      //List<SelenideElement> list = $$(id("org.wikipedia.alpha:id/page_list_item_title")).stream().toList();
+      $$(id("org.wikipedia.alpha:id/page_list_item_image")).stream().findFirst().get().click();
+    });
+
+/*    step("Verify content found", () -> {
+      $(id("pcs-edit-section-title-description")).shouldHave(Condition.text("Island and region in Indonesia"));
+    });*/
 
   }
 }
