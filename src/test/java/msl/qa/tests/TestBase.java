35225230875eab777019d2ef82e780caa1526667
Sveta <ma1m2@mail.ru>
2026-07-01 23:00:37 +0300

    change System.getProperty

--- src/test/java/msl/qa/tests/TestBase.java
+++ src/test/java/msl/qa/tests/TestBase.java
@@ -21,7 +21,7 @@ public class TestBase {
 
   @BeforeAll
   public static void setupEnvironment() {
-    deviceHost = System.getProperty("deviceHost", "emulator");
+    deviceHost = System.getProperty("deviceHost", "browserstack");
     Configuration.browser = getDriverClass(deviceHost);
     Configuration.browserSize = null;
     Configuration.timeout = 30000;
